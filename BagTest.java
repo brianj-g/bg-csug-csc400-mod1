c231afd0984b2742b15b17e4f1378b7abc3fd722
Brian Gunther <brian.gunther@outlook.com>
2025-04-18 12:18:19 -0700

    review pass 1

--- BagTest.java
+++ BagTest.java
@@ -11,15 +11,15 @@ public class BagTest {
 	// Use the Bag's "contains()" method to check that a specific value exists at least once
 	static <T> void containsTest(T myItem, Bag<T> myBag) {
 		if (myBag.contains(myItem)) {
-			System.out.println("Bag contains '" + myItem + "'.");
+			System.out.println("Bag contains '" + myItem + "'");
 		} else {
-			System.out.println("Bag does NOT contain '" + myItem + "'.");
+			System.out.println("Bag does NOT contain '" + myItem + "'");
 		}
 	}
 	
 	// Use the Bag's "count()" method to count occurrences of a specific value
 	static <T> void countTest(T myItem, Bag<T> myBag) {
-		System.out.printf("Bag contains %d occurences of '%s'.\n", myBag.count(myItem), myItem);
+		System.out.printf("Bag contains %d occurences of '%s'\n", myBag.count(myItem), myItem);
 	}
 	
 	// Main function
@@ -57,16 +57,15 @@ public class BagTest {
 		System.out.println();
 		
 		// Remove an item
-		System.out.println("Testing the 'remove()' method:");
-		myBag.remove("is");
-		System.out.println();
+		System.out.println("Removing one occurrence of the word 'test' from the Bag...");
+		myBag.remove("test");
 		System.out.println("New Bag contents:");
 		printBag(myBag);
 		
 		// Check the contains() and count() methods again
 		System.out.println("Testing the successful removal:");
-		containsTest("is", myBag);
-		countTest("is", myBag);
+		containsTest("test", myBag);
+		countTest("test", myBag);
 
 	}
 }
